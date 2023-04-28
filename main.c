a8c1ecef15d0980c573a06c6742c6a27da2c5781
iynix-xinyi <112117461+iynix-xinyi@users.noreply.github.com>
2023-04-28 17:20:25 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,31 @@
+/*
+ * TempAndHumdity.c
+ *
+ * Created: 4/28/2023 4:51:49 PM
+ * Author : Xinyi Yuan
+ */ 
+
+#ifndef F_CPU
+#define F_CPU 16000000UL	// 16 MHz clock speed
+#endif
+
+#include "LCD.h"
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+	_delay_ms(2);
+    lcd_init();
+	_delay_ms(1);
+	lcd_clear();
+	_delay_ms(2);
+    lcd_write_word("Temp: ");
+    lcd_goto_xy(1,0);
+    lcd_write_word("LCD is working");
+	_delay_ms(1);
+	lcd_send_command(0x0E);
+	_delay_ms(2);
+	lcd_send_command(0x0C);
+}
+
