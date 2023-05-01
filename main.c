274b8e69434ca0d95983625c1c403383126c7ad6
iynix-xinyi <112117461+iynix-xinyi@users.noreply.github.com>
2023-05-01 14:25:01 -0500

    Finished

--- main.c
+++ main.c
@@ -21,11 +21,14 @@ void changeTemp(double temp[1]);
 void changeHumi(double hum[1]);
 void printSumError();
 void printTimeError();
-void printNA();
 
+/*************************************************************
+	GLOBAL VARIABLES
+**************************************************************/
 volatile int buttonFlag = 0;		// off
 
 
+/* Interrupt Service Routine for INT0 */
 ISR(INT0_vect) {
 	switchPressed();
 }
@@ -33,30 +36,36 @@ ISR(INT0_vect) {
 
 int main(void)
 {	
-	
-	//PCICR |= 0b00000001;
-	//PCMSK0 |= 0b00000100;
-	
-	//PCICR |= 0b00000100;
-	//PCMSK2 |= 0b00000100;
-	cli();
-	EICRA = EICRA | 0x02; // INT0 if high --> low
-	EIMSK = EIMSK | 1 << INT0; // Enable INT0
-	
+	cli();								// Disable interrupt
+	DDRC |= 0b00000110;					// set led and back light as output
+	EICRA = EICRA | 0x02;				// INT0 if high --> low
+	EIMSK = EIMSK | 1 << INT0;			// Enable INT0
+
 	// LCD setup
 	lcdSetUp();
-	sei();
+	sei();								// Enable interrupt
+	
 	// DHT11 setup
-	int8_t DHTreturnCode;
+	int8_t DHTreturnCode;		
+	// Check if it's reading correctly		
 	while(1) {
 		DHTreturnCode = DHT11ReadData();
-		if (buttonFlag == 1) {
-			if (DHTreturnCode == 1) {
+		if (buttonFlag == 1) {			// if the button status is on
+			if (DHTreturnCode == 1) {	// read it correctly
 				cli();
+				// Display temperature and humidity
 				DHT11DisplayTemperature();
-				DHT11DisplayHumidity();
+				DHT11DisplayHumidity();			
+				// Check if the temperature is over 50C
+				if (currentTemp >= 50) {
+					turnOnLed();
+				}
+				else {
+					turnOffLed();
+				}
 				sei();
 			}
+			// Reading in error
 			else {
 				if (DHTreturnCode == -1) {
 					printSumError();	// print out error starting on (0, 6)
@@ -64,38 +73,30 @@ int main(void)
 				else {
 					printTimeError();	// print out error starting on (0, 6)
 				}
+
 			}
 		}
 	}
 	
 }
 
+
+/*************************************************************
+	FUNCTIONS
+**************************************************************/
+/* Initialize LCD */
 void lcdSetUp(){
 	_delay_ms(2);
 	lcd_init();
 	_delay_ms(1);
 	lcd_clear();
 	_delay_ms(2);
-	lcd_write_word("Temp:NA");
-	lcd_goto_xy(1,0);
-	lcd_write_word("Humidity:NA");
-	_delay_ms(1);
-	lcd_send_command(0x0E);
-	_delay_ms(2);
-	lcd_send_command(0x0C);
+	lcd_send_command(0x0E);				// Cursor no blink
+	_delay_ms(2);				
+	lcd_send_command(0x0C);				// No Cursor
 }
 
-void printNA() {
-	lcd_goto_xy(0,0);
-	_delay_ms(2);
-	lcd_clear();
-	_delay_ms(2);
-	lcd_write_word("Temp:NA");
-	lcd_goto_xy(1,0);
-	lcd_write_word("Humidity:NA");
-	_delay_ms(1);
-}
-
+/* Print out sum error on LCD */
 void printSumError() {
 	lcd_goto_xy(0, 6);
 	_delay_ms(1);
@@ -103,6 +104,7 @@ void printSumError() {
 	_delay_ms(1);
 }
 
+/* Print out time error on LCD */
 void printTimeError() {
 	lcd_goto_xy(0, 6);
 	_delay_ms(1);
@@ -110,15 +112,47 @@ void printTimeError() {
 	_delay_ms(1);
 }
 
+/* Turn on LED */
+void turnOnLed() {
+	unsigned char temp;
+	temp = PORTC;
+	temp = temp | 0b00000010;
+	PORTC = temp;
+	
+}
+
+/* Turn off LED */
+void turnOffLed() {
+	unsigned char temp;
+	temp = PORTC;
+	temp = temp &~(0b00000010);
+	PORTC = temp;
+}
+
+/* if button is off, buttonFlag = 0, turn on back light for LCD.
+	if button is on, buttonFlag = 1, turn off back light for LCD, and clear
+	the lowest temperature stored in DHT11sensor file, clear the LCD screen,
+	turn off LED. */
 void switchPressed() {
 	if (buttonFlag == 0) {
 		buttonFlag = 1;
+		unsigned char temp;
+		// Turn on LCD back light
+		temp = PORTC;
+		temp = temp | 0b00000100;
+		PORTC = temp;
 		return;
 	}
 	else {
 		buttonFlag = 0;
-		clearLow();
-		lcd_clear();
+		turnOffLed();					// Turn off LED
+		clearLow();						// Clear the lowest temperature stored in DHT11 file
+		lcd_clear();					// Clear LCD display
+		unsigned char temp;
+		// Turn off LCD back light
+		temp = PORTC;
+		temp = temp &~(0b00000100);
+		PORTC = temp;
 	}	
 }
 
